106deb0ef1f49babece299dcd5c2ac19b4b04d10
Nuggdr <nuggetmex003@outlook.com>
2024-10-15 13:51:23 -0300

    Update dbConnect.ts

--- lib/dbConnect.ts
+++ lib/dbConnect.ts
@@ -1,26 +1,17 @@
-// lib/dbConnect.ts
 import { MongoClient } from 'mongodb';
 
-const uri = process.env.MONGODB_URI; // Certifique-se de que você está usando a variável de ambiente correta
-const options = {
-    
-};
-
-let client: MongoClient | null = null;
-let clientPromise: Promise<MongoClient> | null = null;
+const uri = process.env.MONGODB_URI; // Substitua pela sua URI
+const options = {};
 
 if (process.env.NODE_ENV === 'development') {
     // Durante o desenvolvimento, usamos o cliente do MongoDB de forma global
     if (!global._mongoClientPromise) {
-        client = new MongoClient(uri, options);
+        const client = new MongoClient(uri, options);
         global._mongoClientPromise = client.connect();
     }
-    clientPromise = global._mongoClientPromise;
+    client = await global._mongoClientPromise;
 } else {
-    // No ambiente de produção, criamos um novo cliente a cada vez
-    client = new MongoClient(uri, options);
-    clientPromise = client.connect();
+    // Em produção, cria uma nova conexão
+    const client = new MongoClient(uri, options);
+    await client.connect();
 }
-
-// Exporta o cliente para ser usado em outras partes da aplicação
-export default clientPromise;
